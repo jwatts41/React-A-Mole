db513849b18bf1799176bdfbd5af3ec9de4d10eb
jwatts41 <wattsjonovan@yahoo.com>
2022-10-28 18:20:50 -0400

    for loop is working

---
+++ react_a_mole/src/components/MoleContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Mole from '../components/Mole'
+
+function MoleContainer(props) {
+  return (
+    <div>
+    <h2> Mole Container </h2>
+    <Mole />
+    </div>
+  )
+}
+
+export default MoleContainer
